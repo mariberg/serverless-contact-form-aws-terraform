38a297299f3647e4e9ff85ad9163f0a75a38a395
Marika <marika.bergman@gmail.com>
2023-11-04 23:41:50 +0000

    add popover when message sent

--- frontend-react/src/components/ContactForm.tsx
+++ frontend-react/src/components/ContactForm.tsx
@@ -8,9 +8,8 @@ import {
   Textarea,
   Button,
   Group,
-  ActionIcon,
   rem,
-  useMantineTheme,
+  Popover
 } from '@mantine/core';
 import { ContactInfo } from './ContactInfo';
 
@@ -38,7 +37,7 @@ const useStyles = createStyles((theme) => ({
 
   description: {
     color: theme.colors[theme.primaryColor][0],
-    maxWidth: rem(300),
+    maxWidth: '300px',
 
     [theme.fn.smallerThan('sm')]: {
       maxWidth: '100%',
@@ -80,6 +79,16 @@ const useStyles = createStyles((theme) => ({
     },
   },
 
+  inputInvalid: {
+    backgroundColor: theme.white,
+    borderColor: theme.colors.red[6], 
+    color: theme.black,
+
+    '&::placeholder': {
+      color: theme.colors.gray[5],
+    },
+  },
+
   inputLabel: {
     color: theme.black,
   },
@@ -98,14 +107,15 @@ interface FormData {
   message: string;
 }
 
+export function ContactUs() {
+  const { classes } = useStyles();
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    name: '',
+    message: '',
+  });
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
-  export function ContactUs() {
-    const { classes } = useStyles();  
-    const [formData, setFormData] = useState<FormData>({
-      email: '',
-      name: '',
-      message: '',
-    });
 
   const handleChange = (field: keyof FormData, value: string) => {
     setFormData((prevData) => ({
@@ -114,11 +124,22 @@ interface FormData {
     }));
   };
 
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (!isEmailValid) {
+      console.error('Invalid email address');
+      return;
+    }
+
     try {
       // Your API endpoint URL
       const apiUrl = 'https://vtr3npv336.execute-api.eu-west-2.amazonaws.com/test'; // Replace with your actual API endpoint URL
+      console.log("this is the api call  now")
 
       // Send the data as JSON
       const response = await fetch(apiUrl, {
@@ -128,8 +149,6 @@ interface FormData {
         },
         body: JSON.stringify(formData),
       });
-
-      console.log(response);
 
       if (response.ok) {
         // Request successful, do something here
@@ -162,9 +181,17 @@ interface FormData {
             label="Email"
             placeholder="your@email.com"
             required
-            classNames={{ input: classes.input, label: classes.inputLabel }}
+            classNames={{
+              input: isEmailValid ? classes.input : classes.inputInvalid,
+              label: classes.inputLabel,
+            }}
             value={formData.email}
-            onChange={(e) => handleChange('email', e.target.value)}
+            onChange={(e) => {
+              const email = e.target.value;
+              setIsEmailValid(validateEmail(email));
+              handleChange('email', email);
+            }}
+            error={!isEmailValid}
           />
           <TextInput
             label="Name"
@@ -186,10 +213,19 @@ interface FormData {
           />
 
           <Group position="right" mt="md">
-            <Button className={classes.control} onClick={handleSubmit}>Send message</Button>
+            <Popover width={200} position="top" withArrow shadow="md">
+              <Popover.Target>
+                <Button className={classes.control} onClick={handleSubmit}>
+                  Send message
+                </Button>
+              </Popover.Target>
+              <Popover.Dropdown>
+                <Text size="m">Your message has been sent!</Text>
+              </Popover.Dropdown>
+            </Popover>
           </Group>
         </div>
-      </SimpleGrid>
+        </SimpleGrid>
     </div>
   );
 }
